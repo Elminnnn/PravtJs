75771676707e2b5466ae4b219886bcf2eb0f37e9
Elminnnn <114989561+Elminnnn@users.noreply.github.com>
2022-12-06 15:42:08 +0400

    Add files via upload

--- pravt/assets/js/app.js
+++ pravt/assets/js/app.js
@@ -1,51 +1,46 @@
-const allBodyImgs=document.querySelector('.allbodyimgs');
-const apiUrl= './data/news.json';
+const allBodyImgs = document.querySelector('.allbodyimgs');
+const apiUrl = './data/news.json';
 
 fetch(apiUrl)
-.then(response =>{
-    if (response.ok) {
-      return response.json();  
-    }else{
-        throw new Error('error');
-    }
-} )
-.then(data =>{
-    addImgs(data);
-})
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('error');
+        }
+    })
+    .then(data => {
+        addImgs(data);
 
-function addImgs(cards){
-    for (let index = 0; index < cards.length; index++){
-        if (index==0 ||index==2) {
-            cards=`<div class="card">
-            <div class=" photo firstimgs">
-                <img class="imgg1" src="${cards[index].url.foreach(url => url.)" alt="e">
-                <img class="imgg2" src="${cards[index].url}" alt="e">
-            </div>
+    })
+
+function addImgs(cardsData) {
+    for (let index = 0; index < cardsData.length; index++) {
+
+        let cards = `<div class="card">
+            <div class=" photo">`
+        for (let j = 0; j < cardsData[index].images.length; j++) {
+            if (cardsData[index].images.length == 1) {
+
+                cards += `<img class="cardImg" src="${cardsData[index].images[j]}" alt="e">`;
+                break;
+            }
+            else {
+                cards += `<img class="imgg${j + 1}" src="${cardsData[index].images[j]}" alt="e">`;
+            }
+        }
+        cards += `  </div >
             <div class="alts">
-                <h2>${cards[index].title}</h2>
+                <h2>${cardsData[index].title}</h2>
             </div>
             <div class="altfooter">
-                <div class="dates">${cards[index].date}</div>
+                <div class="dates">${cardsData[index].date}</div>
                 <a target="_blank" href="https://pravt-nn.ru/news/novinki-amortizatory/">More <img
                         src="./assets/img/Arrow 2.png"> </a>
             </div>
-        </div>`
-        }else{
-            cards=`<div class="card">
-            <div class=" photo secondimgs">
-                <img src="${cards[index].url}" alt="e">
-            </div>
-            <div class="alts">
-                <h2>${cards[index].title}</h2>
-            </div>
-            <div class="altfooter ">
-                <div class="dates">${cards[index].date}</div>
-                <a target="_blank" href="https://pravt-nn.ru/news/novinki-amortizatory/">More <img
-                        src="./assets/img/Arrow 2.png"> </a></span>
-            </div>
+        </div > `
 
-        </div>`
-        }
-        allBodyImgs.innerHTML+=cards; 
+        allBodyImgs.innerHTML += cards;
     }
 }
+
